c835a8c01a4db263985c17bbae939e3e855c7150
Ankit Kumar Shah <97950192+Infinite-Null@users.noreply.github.com>
2023-06-15 00:23:13 +0530

    Update About.js

--- src/components/About.js
+++ src/components/About.js
@@ -7,36 +7,36 @@ export default function About(props) {
                 <div className="accordion-item">
                     <h2 className="accordion-header" id="headingOne">
                         <button className="accordion-button" type="button" data-bs-toggle="collapse" data-bs-target="#collapseOne" aria-expanded="true" aria-controls="collapseOne" style={props.myStyle}>
-                            Accordion Item #1
+                           Name
                         </button>
                     </h2>
                     <div id="collapseOne" className="accordion-collapse collapse show" aria-labelledby="headingOne" data-bs-parent="#accordionExample">
                         <div className="alert alert-primary" role="alert">
-                            A simple primary alert—check it out!
+                           Ankit Kumar Shah
                         </div>
                     </div>
                 </div>
                 <div className="accordion-item">
                     <h2 className="accordion-header" id="headingTwo">
                         <button className="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#collapseTwo" aria-expanded="false" aria-controls="collapseTwo" style={props.myStyle}>
-                            Accordion Item #2
+                           Discription
                         </button>
                     </h2>
                     <div id="collapseTwo" className="accordion-collapse collapse" aria-labelledby="headingTwo" data-bs-parent="#accordionExample">
                         <div className="alert alert-danger" role="alert">
-                            A simple danger alert—check it out!
+                            I am a Bca Student at lovely professional university.
                         </div>
                     </div>
                 </div>
                 <div className="accordion-item">
                     <h2 className="accordion-header" id="headingThree">
                         <button className="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#collapseThree" aria-expanded="false" aria-controls="collapseThree" style={props.myStyle}>
-                            Accordion Item #3
+                           Contact Info
                         </button>
                     </h2>
                     <div id="collapseThree" className="accordion-collapse collapse" aria-labelledby="headingThree" data-bs-parent="#accordionExample">
                         <div className="alert alert-success" role="alert">
-                            A simple danger alert—check it out!
+                            ankit.kum.sha9933@gmail.com
                         </div>
                     </div>
                 </div>
